53e2f880108359ad8c1f8332a6ecfc17352e1920
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-01-12 22:20:02 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import React from 'react';
+import Profile from './Profile';
+import user from './user.json';
+import Statistics from './Statistics'; 
+import data from './data.json';
+import FriendList from './FriendList'; 
+import friends from './friends.json';
+import TransactionHistory from './TransactionHistory';
+import transactions from './transactions.json';
+import '../css/app.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+   <div className='container'>
+      <div>
+            <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+            />
+      </div>
+       <div>
+       <Statistics title="Upload stats" stats={data} />
+      </div>
+      <div>
+         <FriendList friends={friends} />
+         </div>
+         <div>
+         <TransactionHistory items={transactions} />
+         </div>
+      </div>
   );
 };
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import css from '../css/FriendList.module.css'
+
+const FriendListItem = ({ avatar, name, isOnline}) => {
+  return (
+    <li className={css.item}>
+      <span className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
+         <img className={css.avatar} src={avatar} alt="User avatar" width="80" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(friend => (
+        <FriendListItem key={friend.id} {...friend} />
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
